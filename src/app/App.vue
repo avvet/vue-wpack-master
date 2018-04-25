53d7a212fbd5ff3b216030c7ac38a2f75a29b856
avvet <sveta.drozdovich@yandex.ru>
2018-04-25 14:18:15 +0300

    main part made

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
-    <h1>App vue rendered! {{msg}}</h1>
+  <div id="'app">
+    <!--<h1>App vue rendered! {{msg}}</h1>-->
+    <!--<img style="width:140px; margin:14px;" src="./assets/img/logo.png"/>-->
     <image-container></image-container>
-    <img src="../assets/img/familyguy.png" width="100" height="300" alt="">
+    <video-player></video-player>
+    <!--<img src="../assets/img/familyguy.png" width="100" height="300" alt="">-->
   </div>
 </template>
 
 <script>
-  import ImageContainerComponenr from "Components/image-container-component/ImageContainerComponent"
+  import ImageContainerComponent from "Components/image-container-component/ImageContainerComponent"
+  import VideoPlayer from "Components/video-player-component/VideoPlayer.vue";
 
   export default {
     data() {
       return {
-        msg: "DataMsg"
+        // msg: "DataMsg"
       }
     },
+    name: 'App',
     components: {
-      'image-container': ImageContainerComponenr
+      'image-container': ImageContainerComponent,
+      'video-player':VideoPlayer
     }
   }
 </script>
+
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+</style>
